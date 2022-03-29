02bf6534daca6d65447858f421635c1944b6f4e7
Aleksandra <Veyt.AA@gutagroup.ru>
2022-03-29 18:26:12 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Тестовое задание для LEAN-IT</h1>
+  </div>
+  <div class="search">
+    <form id="search">
+      Поиск по таблице <input name="query" v-model="searchQuery" />
+    </form>
+  </div>
+  <myTable
+    :heroes="gridData"
+    :columns="gridColumns"
+    :filter-key="searchQuery"
+  ></myTable>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myTable from './components/myTable/myTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    myTable
+  },
+  data () {
+    return {
+      searchQuery: '',
+      gridColumns: ['name', 'power', 'age', 'city'],
+      gridData: [
+        { name: 'Chuck', power: Infinity, age: 50, city: 'NY' },
+        { name: 'Bruce', power: 9000, age: 40, city: 'Moscow' },
+        { name: 'Jackie', power: 7000, age: 55, city: 'LA' },
+        { name: 'Jet', power: 8000, age: 35, city: 'Tokio' }
+      ]
+    }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./global.scss" lang="scss"></style>
